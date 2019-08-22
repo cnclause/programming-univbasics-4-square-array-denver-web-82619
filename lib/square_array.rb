40f98e40410a0b38f092ca332181378bd71f9d44
Catherine Clause <cnclause123@gmail.com>
2019-08-22 19:06:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,6 +4,7 @@ def square_array(numbers)
 counter = 0
   new_numbers [] 
 while counter < numbers.length do
+  
   puts numbers[counter]
    counter +=1
  return new_numbers [] << "numbers[counter]**2"
